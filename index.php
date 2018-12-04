1dc9a05a621892b2cb674166d532eca3f364761e
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-12-04 19:45:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,17 +6,31 @@
 	<body>
 		<div id="demo"><h2>Hello</h2></div>
 		
-		<form action="GetDataOut.php" method="GET" id="add">
-			
-		</form>
+		<?php
+			include('connection.php');
 		
-		
+			$sqlget = "SELECT * FROM data";
+			$sqldata = mysqli_query($dbcon, $sqlget) or die('error');
+
+			echo "<table>";
+			echo "<tr><th>Beacon name</th><th>seconds</th></tr>";
+
+			while($row = mysqli_fetch_array($sqldata, MYSQLI_ASSOC)){
+
+				echo "<tr><td>";
+				echo $row['beacon_name'];
+				echo "</td><td>";
+				echo $row['seconds'];
+				echo "</td></tr>";
+			}
+
+			echo "</table>";
+		?>
 
 		<button type="button" onclick="loadDoc()">Change Content</button>
 
 		<script src="https://code.jquery.com/jquery-3.1.1.min.js"
 				  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
 				  crossorigin="anonymous"></script>
-		<script> src="global.js"></script>
 	</body>
 </html>
