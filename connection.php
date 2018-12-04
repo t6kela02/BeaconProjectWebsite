058df4ab11ea5c3fbc76cd69ad867d23bb1c8ad6
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-12-04 19:33:16 +0200

    Update and rename GetDataOut.php to connection.php

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+$con = mysqli_connect("den1.mysql3.gear.host", "accountit", "Ru8tmg~976-i", "accountit");
+    
+    $selectUser = $con->prepare("SELECT * FROM `data`");
+    $selectUser->execute();
+    $result = $selectUser->get_result();
+    
+    $response = array();
+    $response["success"] = false;
+    
+    while($row = $result->fetch_assoc())
+    {
+        $response["success"] = true;  
+        $response["data"][] = $row;
+    }
+    
+    echo json_encode($response);
+?>
