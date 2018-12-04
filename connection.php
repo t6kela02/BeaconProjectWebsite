90320d766f324caa773b642ec439e0e0c66f88e3
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-12-04 19:40:21 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,21 +6,4 @@
 	DEFINE ('DB_NAME', 'accountit');
 
     $dbcon = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME);
-
-		$sqlget = "SELECT * FROM data";
-		$sqldata = mysqli_query($dbcon, $sqlget) or die('error');
-
-		echo "<table>";
-		echo "<tr><th>Beacon name</th><th>seconds</th></tr>";
-
-		while($row = mysqli_fetch_array($sqldata, MYSQLI_ASSOC)){
-
-			echo "<tr><td>";
-			echo $row['beacon_name'];
-			echo "</td><td>";
-			echo $row['seconds'];
-			echo "</td></tr>";
-		}
-
-		echo "</table>";
 ?>
