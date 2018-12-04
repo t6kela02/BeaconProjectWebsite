33203c043f84f0063a5762674e58f36fc6147bd2
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-12-04 19:33:58 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,18 +1,26 @@
 <?php
-$con = mysqli_connect("den1.mysql3.gear.host", "accountit", "Ru8tmg~976-i", "accountit");
-    
-    $selectUser = $con->prepare("SELECT * FROM `data`");
-    $selectUser->execute();
-    $result = $selectUser->get_result();
-    
-    $response = array();
-    $response["success"] = false;
-    
-    while($row = $result->fetch_assoc())
-    {
-        $response["success"] = true;  
-        $response["data"][] = $row;
-    }
-    
-    echo json_encode($response);
+
+	DEFINE ('DB_USER', 'accountit');
+	DEFINE ('DB_PSWD', 'Ru8tmg~976-i');
+	DEFINE ('DB_HOST', 'den1.mysql3.gear.host');
+	DEFINE ('DB_NAME', 'accountit');
+
+    $dbcon = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME);
+
+		$sqlget = "SELECT * FROM data";
+		$sqldata = mysqli_query($dbcon, $sqlget) or die('error');
+
+		echo "<table>";
+		echo "<tr><th>Beacon name</th><th>seconds</th></tr>";
+
+		while($row = mysqli_fetch_array($sqldata, MYSQLI_ASSOC)){
+
+			echo "<tr><td>";
+			echo $row['beacon_name'];
+			echo "</td><td>";
+			echo $row['seconds'];
+			echo "</td></tr>";
+		}
+
+		echo "</table>";
 ?>
