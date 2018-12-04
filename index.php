5e8a65a404fd129abb803b598f065f71ff1d63f9
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-12-04 18:47:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Website</title>
-</head>
+	<head>
+		<title>Website</title>
+	</head>
+	<body>
+		<div id="demo"><h2>Hello</h2></div>
+		
+		<form action="GetDataOut.php" method="GET" id="add">
+			
+		</form>
+		
+		
 
-<body>
-	<div id="demo"><h2>Let AJAX change this text</h2></div>
-	
-	<button type="button" onclick="loadDoc()">Change Content</button>
-	
-	<script src="https://code.jquery.com/jquery-3.1.1.min.js"
-			  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-			  crossorigin="anonymous"></script>
-	<script> src="global.js"></script>
-</body>
+		<button type="button" onclick="loadDoc()">Change Content</button>
 
+		<script src="https://code.jquery.com/jquery-3.1.1.min.js"
+				  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+				  crossorigin="anonymous"></script>
+		<script> src="global.js"></script>
+	</body>
 </html>
