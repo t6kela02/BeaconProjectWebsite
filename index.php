5a90a762111fed68bbae99eff185382606437bee
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-12-06 00:32:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
